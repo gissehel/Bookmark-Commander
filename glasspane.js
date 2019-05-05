a599b402c0dadb7e79a0c283739cedee11ba1cc5
jgi <public-vs2015@gissehel.org>
2019-05-05 12:55:43 +0200

    Refactoring : Rename mainSreenContent to mainScreenContent

--- glasspane.js
+++ glasspane.js
@@ -15,31 +15,31 @@ glasspane.init = () => {
         screenParams.shouldReplaceHBar = true;
     }
 
-    let mainSreenContent = '';
+    let mainScreenContent = '';
     //Menu
     //Note the relative position and z-index, without this the dropdown would overlap the menu
     //and menu events would not trigger
-    mainSreenContent += "<span id='menu' class='menu' style='position:relative;z-index:2'>" + ("  Left     File     Command     Options     Right").extend() + "</span>";
-    mainSreenContent += "\n";
+    mainScreenContent += "<span id='menu' class='menu' style='position:relative;z-index:2'>" + ("  Left     File     Command     Options     Right").extend() + "</span>";
+    mainScreenContent += "\n";
 
     //Box Top, missing span here !, it's below now to fill up white spots
-    mainSreenContent += "<span class='border'><span id='h1'><span id='leftroot'></span><span id='riteroot'></span></span>";
-    mainSreenContent += "\n";
+    mainScreenContent += "<span class='border'><span id='h1'><span id='leftroot'></span><span id='riteroot'></span></span>";
+    mainScreenContent += "\n";
 
     //Entries
     for (let counter = 0; counter < screenParams.panelheight; counter++) {
-        mainSreenContent += ("║<span id='left" + counter + "' class='border'>" + " ".repeat(screenParams.panelwidth) + "</span>║");
-        mainSreenContent += ("║<span id='rite" + counter + "' class='border'>" + " ".repeat(screenParams.panelwidth) + "</span>║");
-        mainSreenContent += "\n";
+        mainScreenContent += ("║<span id='left" + counter + "' class='border'>" + " ".repeat(screenParams.panelwidth) + "</span>║");
+        mainScreenContent += ("║<span id='rite" + counter + "' class='border'>" + " ".repeat(screenParams.panelwidth) + "</span>║");
+        mainScreenContent += "\n";
     }
 
     //Box Bottom
-    mainSreenContent += ("<span id='h2'>╠" + "═".repeat(screenParams.panelwidth) + "╩╩" + "═".repeat(screenParams.panelwidth) + "╣</span>");
-    mainSreenContent += "\n";
-    mainSreenContent += ("║<span id='url'>" + " ".repeat((screenParams.panelwidth + 1) * 2) + "</span>║");
-    mainSreenContent += "\n";
-    mainSreenContent += ("<span id='h3'>╚" + "═".repeat((screenParams.panelwidth + 1) * 2) + "╝" + "</span></span>");
-    mainSreenContent += "\n";
+    mainScreenContent += ("<span id='h2'>╠" + "═".repeat(screenParams.panelwidth) + "╩╩" + "═".repeat(screenParams.panelwidth) + "╣</span>");
+    mainScreenContent += "\n";
+    mainScreenContent += ("║<span id='url'>" + " ".repeat((screenParams.panelwidth + 1) * 2) + "</span>║");
+    mainScreenContent += "\n";
+    mainScreenContent += ("<span id='h3'>╚" + "═".repeat((screenParams.panelwidth + 1) * 2) + "╝" + "</span></span>");
+    mainScreenContent += "\n";
 
     const function_keys =
         [
@@ -58,12 +58,12 @@ glasspane.init = () => {
 
     for (key in function_keys) {
         const f = function_keys[key];
-        mainSreenContent += ("<span class='fcode'>F" + f.id + "</span><span class='menu' id='f" + f.id + "'>" + f.description + "</span><span class='fcode'> </span>");
+        mainScreenContent += ("<span class='fcode'>F" + f.id + "</span><span class='menu' id='f" + f.id + "'>" + f.description + "</span><span class='fcode'> </span>");
     }
-    mainSreenContent += ("<span id='end' class='fcode'>" + " ".repeat(screenParams.screenwidth - 91) + "</span>");
-    mainSreenContent += "\n";
+    mainScreenContent += ("<span id='end' class='fcode'>" + " ".repeat(screenParams.screenwidth - 91) + "</span>");
+    mainScreenContent += "\n";
 
-    window.mainScreen.innerHTML = mainSreenContent;
+    window.mainScreen.innerHTML = mainScreenContent;
     mouse.reinit_if_already_init();
     commander.boot();
 
